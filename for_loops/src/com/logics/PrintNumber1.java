b595a2c7fa5f13786a293eda2834ae5ca611e1ad
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2025-03-10 19:49:09 +0530

    Add files via upload

---
+++ for_loops/src/com/logics/PrintNumber1.java
@@ -0,0 +1,21 @@
+package com.logics;
+
+//Print numbers from 1 to 10.
+//o/p  1 2 3 4 5 6 7 8 9 10 
+
+public class PrintNumber1 {
+	static void printNumber()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		printNumber();
+
+	}
+
+}
